eb2b60b55584a44c98a60351f3f558d48327398f
w71370218 <w7147370218@gmail.com>
2022-11-14 17:09:54 +0800

    202211141709 test

--- pages/index.js
+++ pages/index.js
@@ -41,7 +41,8 @@ export async function getServerSideProps(context) {
   let allPostNum = await Posts.countDocuments({});
   const posts = [];
   let skip_postNum = 0
-  const session = await getSession();
+  const { req } = context;
+  const session = await getSession({ req });
   let user = null;
   if (session) {
     user = session.user;
@@ -78,6 +79,21 @@ export async function getServerSideProps(context) {
       postsQ[i].content = postsQ[i].content.replace(/!\[](.+)/g, ' ')
         .replace(/<video.+<\/video>/g, ' ')
         .substring(0, 300);
+
+      //tags
+      for (let j = 0; j < postsQ[i].tags.length; j++) {
+        const tag = await Tags.findOne({ _id: postsQ[i].tags[j] }).select('id name').lean();
+        tag._id = tag._id.toString()
+        postsQ[i].tags[j] = tag;
+      }
+      // series
+      if (postsQ[i].series && postsQ[i].series !== null) {
+        if (postsQ[i].series.hasOwnProperty("id")) {
+          const series = await Series.findOne({ _id: postsQ[i].series.id }).lean();
+          postsQ[i].series = series;
+          postsQ[i].series._id = postsQ[i].series._id.toString();
+        }
+      }
     }
     skip_postNum++;
 
