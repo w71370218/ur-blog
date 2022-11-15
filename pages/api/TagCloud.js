f847e63fe4bebc75a4396ae8baa1f873021e3a9c
w71370218 <w7147370218@gmail.com>
2022-11-15 19:38:44 +0800

    unfinish 202211151938 test: tag cloud

--- pages/api/TagCloud.js
+++ pages/api/TagCloud.js
@@ -2,13 +2,13 @@ import connect from '../../lib/connect';
 import Posts from '../../models/posts';
 import Tags from '../../models/tags';
 
-
+connect();
 
 export default async function handler(req, res) {
     try {
-        connect();
         const q_tags = await Tags.find({}).lean();
         let tags = []
+        /*
         for (let i = 0; i < q_tags.length; i++) {
             const query = { '$or': [{ "tags": q_tags[i] }] }
             const allPostNum = await Posts.countDocuments(query);
@@ -17,6 +17,7 @@ export default async function handler(req, res) {
             tags[i].id = q_tags[i].id
             tags[i].allPostNum = allPostNum;
         }
+        */
         //tags.sort((a, b) => (a.allPostNum < b.allPostNum) ? 1 : ((b.allPostNum < a.allPostNum) ? -1 : 0))
         tags = tags.slice(0, 3)
         /*
