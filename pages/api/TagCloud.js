e612379005f87683703439cd7f9b41d5462b810d
w71370218 <w7147370218@gmail.com>
2022-11-15 22:05:07 +0800

    unfinish 202211152205 test: tag cloud

--- pages/api/TagCloud.js
+++ pages/api/TagCloud.js
@@ -7,35 +7,36 @@ export default async function handler(req, res) {
         const tagsdb = database.collection('tags')
         const postsdb = database.collection('posts')
 
-        const q_tags = tagsdb.find({}).toArray();
+        const q_tags = await tagsdb.find({}).toArray();
         let tags = []
 
-        for (let tag of await q_tags) {
+        for (let tag of q_tags) {
             const query = { '$or': [{ "tags": tag }] }
-            const allPostNum = await postsdb.countDocuments(query);
+            //const allPostNum = await postsdb.countDocuments(query);
             let n_tag = {}
             n_tag.name = tag.name
             n_tag.id = tag.id
-            n_tag.allPostNum = allPostNum.toString();
+            //n_tag.allPostNum = allPostNum;
             tags.push(n_tag)
         }
-
-        tags.sort((a, b) => (a.allPostNum < b.allPostNum) ? 1 : ((b.allPostNum < a.allPostNum) ? -1 : 0))
-        tags = tags.slice(0, 3)
-        const q1 = tags[~~(tags.length / 3 * 1) - 1].allPostNum
-        const q2 = tags[~~(tags.length / 3 * 2) - 1].allPostNum
-        const q3 = tags[~~(tags.length / 3 * 3) - 1].allPostNum
-        tags.forEach(tag => {
-            if (tag.allPostNum >= q1) {
-                tag.size = "4"
-            }
-            else if (tag.allPostNum >= q2) {
-                tag.size = "5"
-            }
-            else {
-                tag.size = "6"
-            }
-        })
+        /*
+                tags.sort((a, b) => (a.allPostNum < b.allPostNum) ? 1 : ((b.allPostNum < a.allPostNum) ? -1 : 0))
+                tags = tags.slice(0, 15)
+                const q1 = tags[~~(tags.length / 3 * 1) - 1].allPostNum
+                const q2 = tags[~~(tags.length / 3 * 2) - 1].allPostNum
+                const q3 = tags[~~(tags.length / 3 * 3) - 1].allPostNum
+                tags.forEach(tag => {
+                    if (tag.allPostNum >= q1) {
+                        tag.size = "4"
+                    }
+                    else if (tag.allPostNum >= q2) {
+                        tag.size = "5"
+                    }
+                    else {
+                        tag.size = "6"
+                    }
+                })
+                */
         res.status(200).json({ tags: tags })
     }
     catch (e) {
